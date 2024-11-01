1cc7dcbdd2ecaacb3f3c7874c9247a605512d2b2
abhi <ugabhi@proton.me>
2024-11-01 21:51:43 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -1,35 +1,23 @@
 {
   description = "Rust flake templalte";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
   };
-
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-
-      # Executed by `nix flake check`
-      checks.${system}.${manifest.name} = pkgs.callPackage ./nix/default.nix {
-        # Force `doCheck` to true, overriding default setting
-        doCheck = pkgs.lib.mkForce true;
-      };
-      # Executed by `nix build .#<name>`
-      packages.${system} = {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
       ${manifest.name} = pkgs.callPackage ./nix/default.nix;
-      # Executed by `nix build .`
       default = pkgs.callPackage ./nix/default.nix;
-      };
-
-      # Formatter (alejandra, nixfmt or nixpkgs-fmt)
-      formatter.${system} = pkgs.alejandra;
-
-      homeManagerModules.${manifest.name} = pkgs.callPackage ./nix/home-module.nix;
-      homeManagerModules.default = self.homeManagerModules.${manifest.name};
-
-      devShells.${system}.default = pkgs.callPackage ./nix/shell.nix { };      
     };
+    formatter.${system} = pkgs.alejandra;
+    homeManagerModules.${manifest.name} = pkgs.callPackage ./nix/home-module.nix;
+    homeManagerModules.default = self.homeManagerModules.${manifest.name};
+    devShells.${system}.default = pkgs.callPackage ./nix/shell.nix {};
+  };
 }
