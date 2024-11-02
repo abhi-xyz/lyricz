ea391b72fdec5b6cd528f3800e00e9f64eee68a3
abhi <ugabhi@proton.me>
2024-11-02 10:50:41 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -16,7 +16,44 @@
         default = self.packages.${system}.${manifest.name};
       };
       formatter.${system} = pkgs.alejandra;
-      homeManagerModules.${manifest.name} = pkgs.callPackage ./nix/home-module.nix { };
+      homeManagerModules.${manifest.name} = { config, pkgs, lib, ... }:
+        let
+          tomlFormat = pkgs.formats.toml { };
+        in
+          {
+          options.program.${manifest.name} = {
+            enable = lib.mkEnableOption "Enable the ${manifest.name} program";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = pkgs.callPackage ./default.nix { };
+              description = "The ${manifest.name} package to use.";
+            };
+            settings = lib.mkOption {
+              type = tomlFormat.type;
+              default = { };
+              example = lib.literalExpression ''
+                [directories]
+                images_path = "/home/abhi/pics/pictures/images"
+
+                [input]
+                dirs = [
+                  "/home/abhi/videos",
+                ]
+              '';
+              description = ''
+                Configuration written to {file}`$XDG_CONFIG_HOME/${manifest.name}/config.json`.
+              '';
+            };
+          };
+          config = lib.mkIf config.program.${manifest.name}.enable {
+            home.packages = [ config.program.${manifest.name}.package ];
+
+            xdg.configFile."${manifest.name}/config.toml" = lib.mkIf (config.program.${manifest.name}.settings != { }) {
+              source = tomlFormat.generate "config.toml" config.program.${manifest.name}.settings;
+            };
+          };
+        };
       homeManagerModules.default = self.homeManagerModules.${manifest.name};
       devShells.${system}.default = pkgs.callPackage ./nix/shell.nix {};
     };
